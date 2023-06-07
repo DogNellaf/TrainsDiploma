e3bea300030b539f16ce506d9b63e40cc55b4aa7
DogNellaf <dans-host@ya.ru>
2023-06-07 17:51:52 +0300

    add new controller

---
+++ src/API/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using TrainsClasses;
+
+namespace API.Controllers
+{
+    [Route("/user")]
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: UserController
+        [HttpGet]
+        public List<User> Users()
+        {
+            return _userService.Users;
+        }
+
+        // GET: user/5
+        [HttpGet("{id:int}")]
+        public IActionResult Details(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+
+            var user = _userService.GetUser(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // POST: user
+        [HttpPost("")]
+        public ActionResult Create(User user)
+        {
+            if (!_userService.Validate(user))
+            {
+                return BadRequest("Данные пользователя некорректны");
+            }
+            user = _userService.Add(user);
+            return Ok(user);
+        }
+
+        // PUT: user/5
+        [HttpPut("{id:int}")]
+        public ActionResult Edit(int id)
+        {
+            return Ok("Test Edit");
+        }
+
+        // DELETE: user/5
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok("Test Delete");
+        }
+    }
+}
---
+++ src/API/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantsClasses;
+using TrainsClasses;
+
+namespace API.Services
+{
+    // сервис взаимодействия с низкоуровневыми классами для пользователей
+    public class UserService
+    {
+        // список всех пользователей
+        public List<User> Users => Database.GetObject<User>();
+
+        // получить пользователя по id
+        public User? GetUser(int id) => Database.GetObject<User>($"id = {id}", "User").FirstOrDefault();
+
+        // проверить корректность введенного пароля
+        public bool CheckPassword(int id, string password)
+        {
+            var client = Database.GetObject<User>($"id = '{id}'").FirstOrDefault();
+
+            if (client is null)
+                return false;
+
+            return Encoder.CheckHash(password, client.Token);
+        }
+
+        // валидация данных нового пользователя
+        public bool Validate(User user)
+        {
+            return true;
+        }
+
+        // добавление пользователя
+        public User Add(User user)
+        {
+            return Database.CreateUser(user.Login, user.Token, user.Balance, user.Role);
+        }
+    }
+}
