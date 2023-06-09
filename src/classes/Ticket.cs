724ee9ecce9044246049c7c19dbb037dde428fe7
DogNellaf <dans-host@ya.ru>
2023-06-09 15:27:47 +0300

    Класс билета

---
+++ src/classes/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace TrainsClasses
+{
+    [DataContract]
+    public class Ticket : Model
+    {
+        [DataMember]
+        public DateTime BuyTime { get; set; }
+        [DataMember]
+        public float Price { get; set; }
+        [DataMember]
+        public int RouteId { get; set; }
+
+        public Ticket() : base(0)
+        {
+
+        }
+
+        public Ticket(object[] items) : base((int)items[0])
+        {
+            BuyTime = DateTime.Parse(items[1].ToString());
+            Price = float.Parse(items[2].ToString());
+            RouteId = (int)items[3];
+        }
+
+        public Ticket(int id, DateTime buyTime, float price, int routeId, string arrivalCity) : base(id)
+        {
+            BuyTime = buyTime;
+            Price = price;
+            RouteId = routeId;
+        }
+    }
+}
