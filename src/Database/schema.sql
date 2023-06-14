c2284bcb9b983f4474e0fa0e5d3c5238266408a3
DogNellaf <dans-host@ya.ru>
2023-06-14 14:00:04 +0300

    Фикс схемы

--- src/Database/schema.sql
+++ src/Database/schema.sql
@@ -5,12 +5,12 @@ GO
 USE [TrainsDiploma]
 GO
 CREATE TABLE [dbo].[City](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [nchar](100) NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Name] [nchar](100) NOT NULL,
  CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Role]    Script Date: 11.06.2023 12:49:51 ******/
@@ -19,14 +19,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Role](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [nchar](20) NOT NULL,
-	[IsWorker] [bit] NOT NULL,
-	[IsAdmin] [bit] NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Name] [nchar](20) NOT NULL,
+  [IsWorker] [bit] NOT NULL,
+  [IsAdmin] [bit] NOT NULL,
  CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Route]    Script Date: 11.06.2023 12:49:51 ******/
@@ -35,16 +35,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Route](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[DepartureTime] [datetime] NOT NULL,
-	[DepartureCityId] [int] NOT NULL,
-	[Duration] [int] NOT NULL,
-	[ArrivalCityId] [int] NOT NULL,
-	[Price] [float] NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [DepartureTime] [datetime] NOT NULL,
+  [DepartureCityId] [int] NOT NULL,
+  [Duration] [int] NOT NULL,
+  [ArrivalCityId] [int] NOT NULL,
+  [Price] [float] NOT NULL,
  CONSTRAINT [PK_Route] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Status]    Script Date: 11.06.2023 12:49:51 ******/
@@ -53,12 +53,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Status](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [nchar](50) NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Name] [nchar](50) NOT NULL,
  CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Ticket]    Script Date: 11.06.2023 12:49:51 ******/
@@ -67,16 +67,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Ticket](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[BuyTime] [datetime] NOT NULL,
-	[Price] [float] NOT NULL,
-	[RouteId] [int] NOT NULL,
-	[StatusId] [int] NOT NULL,
-	[UserId] [int] NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [BuyTime] [datetime] NOT NULL,
+  [Price] [float] NOT NULL,
+  [RouteId] [int] NOT NULL,
+  [StatusId] [int] NOT NULL,
+  [UserId] [int] NOT NULL,
  CONSTRAINT [PK_Ticket] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Transaction]    Script Date: 11.06.2023 12:49:51 ******/
@@ -85,17 +85,17 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Transaction](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserId] [int] NOT NULL,
-	[Value] [float] NOT NULL,
-	[IsComplited] [bit] NOT NULL,
-	[PaymentTime] [datetime] NOT NULL,
-	[PaymentType] [nchar](20) NOT NULL,
-	[Comment] [ntext] NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [UserId] [int] NOT NULL,
+  [Value] [float] NOT NULL,
+  [IsComplited] [bit] NOT NULL,
+  [PaymentTime] [datetime] NOT NULL,
+  [PaymentType] [nchar](20) NOT NULL,
+  [Comment] [ntext] NOT NULL,
  CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[User]    Script Date: 11.06.2023 12:49:51 ******/
@@ -104,17 +104,17 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[User](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Login] [nchar](50) NOT NULL,
-	[Token] [ntext] NULL,
-	[Balance] [float] NOT NULL,
-	[RoleId] [int] NOT NULL,
-	[PassportSeries] [nchar](4) NOT NULL,
-	[PassportNumber] [nchar](6) NOT NULL,
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Login] [nchar](50) NOT NULL,
+  [Token] [ntext] NULL,
+  [Balance] [float] NOT NULL,
+  [RoleId] [int] NOT NULL,
+  [PassportSeries] [nchar](4) NOT NULL,
+  [PassportNumber] [nchar](6) NOT NULL,
  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Transaction] ADD  CONSTRAINT [DF_Transaction_IsComplited]  DEFAULT ((0)) FOR [IsComplited]
@@ -125,11 +125,6 @@ ALTER TABLE [dbo].[Route]  WITH CHECK ADD  CONSTRAINT [FK_Route_City] FOREIGN KE
 REFERENCES [dbo].[City] ([Id])
 GO
 ALTER TABLE [dbo].[Route] CHECK CONSTRAINT [FK_Route_City]
-GO
-ALTER TABLE [dbo].[Route]  WITH CHECK ADD  CONSTRAINT [FK_Route_City1] FOREIGN KEY([DepartureCityId])
-REFERENCES [dbo].[City] ([Id])
-GO
-ALTER TABLE [dbo].[Route] CHECK CONSTRAINT [FK_Route_City1]
 GO
 ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_Route] FOREIGN KEY([RouteId])
 REFERENCES [dbo].[Route] ([Id])
