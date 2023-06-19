4b411ff218e6d7f7ac3ef16220eb081fda2c0f9b
DogNellaf <dans-host@ya.ru>
2023-06-19 23:55:38 +0300

    Почистил схему

--- src/Database/schema.sql
+++ src/Database/schema.sql
@@ -1,108 +1,11 @@
-USE [master]
-GO
-CREATE DATABASE [TrainsDiploma]
-GO
-USE [TrainsDiploma]
-GO
-CREATE TABLE [dbo].[City](
-  [Id] [int] IDENTITY(1,1) NOT NULL,
-  [Name] [nchar](100) NOT NULL,
- CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Role]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[Role](
   [Id] [int] IDENTITY(1,1) NOT NULL,
   [Name] [nchar](20) NOT NULL,
   [IsWorker] [bit] NOT NULL,
   [IsAdmin] [bit] NOT NULL,
- CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Route]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Route](
-  [Id] [int] IDENTITY(1,1) NOT NULL,
-  [DepartureTime] [datetime] NOT NULL,
-  [DepartureCityId] [int] NOT NULL,
-  [Duration] [int] NOT NULL,
-  [ArrivalCityId] [int] NOT NULL,
-  [Price] [float] NOT NULL,
- CONSTRAINT [PK_Route] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Status]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Status](
-  [Id] [int] IDENTITY(1,1) NOT NULL,
-  [Name] [nchar](50) NOT NULL,
- CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Ticket]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Ticket](
-  [Id] [int] IDENTITY(1,1) NOT NULL,
-  [BuyTime] [datetime] NOT NULL,
-  [Price] [float] NOT NULL,
-  [RouteId] [int] NOT NULL,
-  [StatusId] [int] NOT NULL,
-  [UserId] [int] NOT NULL,
- CONSTRAINT [PK_Ticket] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Transaction]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Transaction](
-  [Id] [int] IDENTITY(1,1) NOT NULL,
-  [UserId] [int] NOT NULL,
-  [Value] [float] NOT NULL,
-  [IsComplited] [bit] NOT NULL,
-  [PaymentTime] [datetime] NOT NULL,
-  [PaymentType] [nchar](20) NOT NULL,
-  [Comment] [ntext] NOT NULL,
- CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[User]    Script Date: 11.06.2023 12:49:51 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+  CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
 CREATE TABLE [dbo].[User](
   [Id] [int] IDENTITY(1,1) NOT NULL,
   [Login] [nchar](50) NOT NULL,
@@ -111,47 +14,56 @@ CREATE TABLE [dbo].[User](
   [RoleId] [int] NOT NULL,
   [PassportSeries] [nchar](4) NOT NULL,
   [PassportNumber] [nchar](6) NOT NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-  [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Transaction] ADD  CONSTRAINT [DF_Transaction_IsComplited]  DEFAULT ((0)) FOR [IsComplited]
-GO
-ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_Balance]  DEFAULT ((0)) FOR [Balance]
-GO
-ALTER TABLE [dbo].[Route]  WITH CHECK ADD  CONSTRAINT [FK_Route_City] FOREIGN KEY([ArrivalCityId])
-REFERENCES [dbo].[City] ([Id])
-GO
-ALTER TABLE [dbo].[Route] CHECK CONSTRAINT [FK_Route_City]
-GO
-ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_Route] FOREIGN KEY([RouteId])
-REFERENCES [dbo].[Route] ([Id])
-GO
-ALTER TABLE [dbo].[Ticket] CHECK CONSTRAINT [FK_Ticket_Route]
-GO
-ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_Status] FOREIGN KEY([StatusId])
-REFERENCES [dbo].[Status] ([Id])
-GO
-ALTER TABLE [dbo].[Ticket] CHECK CONSTRAINT [FK_Ticket_Status]
-GO
-ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-GO
-ALTER TABLE [dbo].[Ticket] CHECK CONSTRAINT [FK_Ticket_User]
-GO
-ALTER TABLE [dbo].[Transaction]  WITH CHECK ADD  CONSTRAINT [FK_Transaction_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-GO
-ALTER TABLE [dbo].[Transaction] CHECK CONSTRAINT [FK_Transaction_User]
-GO
-ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Role] FOREIGN KEY([RoleId])
-REFERENCES [dbo].[Role] ([Id])
-GO
-ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Role]
-GO
-USE [master]
-GO
-ALTER DATABASE [TrainsDiploma] SET  READ_WRITE 
-GO
+  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT [DF_User_Balance]  DEFAULT ((0)) FOR [Balance],
+  CONSTRAINT [FK_User_Role] FOREIGN KEY([RoleId])
+);
+
+CREATE TABLE [dbo].[Transaction](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [UserId] [int] NOT NULL,
+  [Value] [float] NOT NULL,
+  [IsComplited] [bit] NOT NULL,
+  [PaymentTime] [datetime] NOT NULL,
+  [PaymentType] [nchar](20) NOT NULL,
+  [Comment] [ntext] NOT NULL,
+  CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT [DF_Transaction_IsComplited]  DEFAULT ((0)) FOR [IsComplited],
+  CONSTRAINT [FK_Transaction_User] FOREIGN KEY([UserId])
+);
+
+CREATE TABLE [dbo].[City](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Name] [nchar](100) NOT NULL,
+  CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Status](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Name] [nchar](50) NOT NULL,
+  CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Route](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [DepartureTime] [datetime] NOT NULL,
+  [DepartureCityId] [int] NOT NULL,
+  [Duration] [int] NOT NULL,
+  [ArrivalCityId] [int] NOT NULL,
+  [Price] [float] NOT NULL,
+  CONSTRAINT [PK_Route] PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT [FK_Route_City] FOREIGN KEY([ArrivalCityId]),
+);
+
+CREATE TABLE [dbo].[Ticket](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [BuyTime] [datetime] NOT NULL,
+  [Price] [float] NOT NULL,
+  [RouteId] [int] NOT NULL,
+  [StatusId] [int] NOT NULL,
+  [UserId] [int] NOT NULL,
+  CONSTRAINT [PK_Ticket] PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT [FK_Ticket_Route] FOREIGN KEY([RouteId]),
+  CONSTRAINT [FK_Ticket_Status] FOREIGN KEY([StatusId]),
+  CONSTRAINT [FK_Ticket_User] FOREIGN KEY([UserId])
+);
